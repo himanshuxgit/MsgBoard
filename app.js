2cbc312a3b8ccefcc286aca95d80c9c44a811b74
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-09-06 15:36:19 +0530

    integrated MongoDB Atlas

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
+// MongoDB Atlas connection URL loaded from environment variables
+const mongoURL = process.env.MONGODB_URI;
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Mini Messageboard', messages });
+// Create a MongoDB client
+const client = new MongoClient(mongoURL);
+
+// Connect to the MongoDB Atlas cluster
+client.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
+  console.log('Connected to MongoDB Atlas');
+});
+
+// MongoDB collection for messages
+const db = client.db('messageBoard'); // Replace 'messageBoard' with your preferred database name
+const messagesCollection = db.collection('messages');
+
+// Create an index on the 'added' field (descending order)
+messagesCollection.createIndex({ added: -1 }, (err, result) => {
+  if (err) {
+    console.error('Error creating index:', err);
+  } else {
+    console.log('Index created:', result);
+  }
+});
+
+
+app.get('/', async (req, res) => {
+  try {
+    const messages = await messagesCollection.find().toArray();
+    res.render('index', { title: 'Mini Messageboard', messages });
+  } catch (err) {
+    console.error('Error fetching messages:', err);
+    res.status(500).send('Error fetching messages');
+  }
 });
 
 app.get('/new', (req, res) => {
   res.render('form', { title: 'New Message' });
 });
 
-app.post('/new', (req, res) => {
+app.post('/new', async (req, res) => {
   const { messageText, messageUser } = req.body;
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
-  res.redirect('/');
+  const newMessage = {
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  };
+
+  try {
+    await messagesCollection.insertOne(newMessage);
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error inserting message:', err);
+    res.status(500).send('Error inserting message');
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-module.exports = app;
+module.exports = app; 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Import the MongoDB client and collection (defined in app.js)
+const { client, messagesCollection } = require('../app');
+
+// Route for the message board home page
+router.get('/', async (req, res) => {
+  try {
+    const messages = await messagesCollection.find().toArray();
+    res.render('index', { title: 'Mini Messageboard', messages });
+  } catch (err) {
+    console.error('Error fetching messages:', err);
+    res.status(500).send('Error fetching messages');
+  }
+});
+
+// Route for the new message form
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'New Message' });
+});
+
+// Route for submitting a new message
+router.post('/new', async (req, res) => {
+  const { messageText, messageUser } = req.body;
+  const newMessage = {
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  };
+
+  try {
+    await messagesCollection.insertOne(newMessage);
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error inserting message:', err);
+    res.status(500).send('Error inserting message');
+  }
 });
 
 module.exports = router;

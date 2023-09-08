a72c47eb9a348c2abeebd9ff4c286c0f69c4505d
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-09-08 23:38:11 +0530

    minor changes

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -9,4 +9,23 @@
       container.style.height = container.offsetWidth + 'px';
     });
   });
-  
+  function deleteMessage(messageId) {
+    if (confirm("Are you sure you want to delete this message?")) {
+      fetch('/messages/' + messageId, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            window.location.reload();
+          } else {
+            console.error('Failed to delete message:', response.status);
+          }
+        })
+        .catch((error) => {
+          console.error('Error deleting message:', error);
+        });
+    }
+  }
